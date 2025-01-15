579a59051e90bba07da5ebd51b0c200a005cd1a3
Stephen Blythe <sblythe@lindenwood.edu>
2025-01-15 11:53:51 -0600

    Updated code to add command line arguments with polymorphism

--- CommandLine/CommandLine.cpp
+++ CommandLine/CommandLine.cpp
@@ -2,10 +2,40 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+string addArgs(char *arr[], int nels)
 {
-
+  string sum="";
   
+  for (int index=1; index < nels; index++)
+    sum += arr[index];
 
+  return sum;
+}
+
+int addArgs(int nels, char *arr[])
+{
+  int sum=0;
+  
+  for (int index=1; index < nels; index++)
+    sum += stoi(arr[index]);
+
+  return sum;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+  cout << "Hello World" << endl;
+
+  /*
+  for (int index=1; index < argc; index++)
+    sum += stoi(argv[index]);
+    //cout << argv[index] << endl;
+    */
+  
+  cout << "Total string sum is:" << addArgs(argv, argc) << endl;
+  cout << "Total int sum is:" << addArgs(argc, argv) << endl;
+  
   return 0;
 }

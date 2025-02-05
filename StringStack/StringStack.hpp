684ab08baec1819d7384d835453f3e00dbb41c3b
Stephen Blythe <sblythe@lindenwood.edu>
2025-02-05 11:54:09 -0600

    Added StringStack class

---
+++ StringStack/StringStack.hpp
@@ -0,0 +1,19 @@
+#ifndef _STRING_STACK_HPP_
+#define _STRING_STACK_HPP_
+
+#include <list>
+#include <string>
+
+class StringStack: private std::list<std::string>
+{
+public:
+  StringStack(); // automatically calls list<String> constructor
+
+  void push(const std::string &s);
+  std::string pop();
+  const std::string& top() const;
+  bool isEmpty() const;
+};
+
+
+#endif

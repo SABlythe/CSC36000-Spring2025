8f54242e0dbf00a5c4e0c018bb19822b4d50e2b3
Stephen Blythe <sblythe@lindenwood.edu>
2025-03-31 10:59:20 -0500

    Added Fibonacci motivator for dynamic programming.

---
+++ DynamicProgramming/Fibonacci/fibo1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+long long int fibo(int seqNum)
+{
+  if (seqNum<=1)
+    return 1;
+
+  return fibo(seqNum-1) + fibo(seqNum-2);
+}
+
+int main(int argc, char *argv[])
+{
+  int snum = stoi(argv[1]);
+  cout << fibo(snum) << endl;
+  return 0;
+}
